1a1e1d38362a16363206f3eccec6b6d66f8a6b24
Pilot110 <bambangdarmawan2022@gmail.com>
2026-04-04 14:52:16 +0700

    Tambah folder Week 5 Modul 5

---
+++ Week 5- Modul 5/guided/guided1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+func main() {
+	var n int
+	fmt.Scan(&n)
+	baris (n)
+}
+
+func baris (bilangan int) {
+	if bilangan == 1 {
+		fmt.Print("1")
+	} else {
+		fmt.Print(bilangan)
+		baris (bilangan - 1)
+	}
+}
